a87ceb45be9b55097e56a304b0d529e0cc226499
refute2change <madguyinthehouse@gmail.com>
2025-09-15 10:20:33 +0700

    minor bug fixes

--- scripts.js
+++ scripts.js
@@ -96,7 +96,7 @@ function setMsg(t){ msgEl.textContent = t; }
 playBtn.onclick = playMove;
 inputEl.onkeydown = e => { if (e.key==='Enter') playMove(); };
 resetBtn.onclick = () => {
-    readWordsFromFile();
+    readWordsfromFile();
     const words = builtin.slice();
     const startIdx = Math.floor(Math.random() * words.length);
     let endIdx;
